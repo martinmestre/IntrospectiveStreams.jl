bf581f260fb494357baecabc9cc412f3a492c730
Martín Mestre <mmestre@fcaglp.unlp.edu.ar>
2023-05-11 19:15:22 -0300

    example.jl

---
+++ src/example.jl
@@ -0,0 +1,10 @@
+using Pkg; Pkg.activate(".")
+using IntrospectiveStreams
+
+name_s = ["GD-1", "Pal5", "PS1-A", "Jhelum", "Fjorm-M68"]
+
+file_orig, file_corr, file_phot = name_files_all(name_s)
+
+# correct_extinction_Gaia_loop(name_s, file_orig, file_corr)
+
+println("Tasks performed.")
