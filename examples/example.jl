f2b593093dcb5cd55ae830de6d63c5be5e6264ac
Martín Mestre <mmestre@fcaglp.unlp.edu.ar>
2023-05-18 00:28:15 -0300

    Midnight commit !

--- examples/example.jl
+++ examples/example.jl
@@ -2,13 +2,13 @@
 """Example of package usage."""
 function example()
     name_s = ["GD-1", "Pal5", "PS1-A", "Jhelum", "Fjorm-M68"]
-    name_t = ["GD-1-PB18", "Pal5-PW19", "Jhelum-b-B19", "PS1-A-B16", "M68-P19"]
+    name_t = ["GD-1-PB18", "Pal5-PW19", "PS1-A-B16", "Jhelum-b-B19", "M68-P19"]
     age    = [12.0, 12.0, 12.0, 12.0 ,11.2]*10^9 #yr
     metal  = [-1.5, -1.4, -1.7, -1.2, -2.2]
-    name_s = ["GD-1"]
-    name_t = ["GD-1-PB18"]
-    age    = [12.0]*10^9
-    metal  = [-1.5]
+    # name_s = ["GD-1"]
+    # name_t = ["GD-1-PB18"]
+    # age    = [12.0]*10^9
+    # metal  = [-1.5]
     filters = fill("UBVRIplus",length(name_s))
     tol_curation = [0.3, 0.3, 0.3]  # for all the streams the same tolerances in μ_α*cosδ, μ_δ, Π.
     col_bounds = (-0.75, 2.0)
