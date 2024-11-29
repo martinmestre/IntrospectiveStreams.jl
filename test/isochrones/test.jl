8586665f8cb2618050cc744ece447dbb4dfe3f0c
Martín Mestre <mmestre@fcaglp.unlp.edu.ar>
2024-11-29 17:45:54 -0300

    Improving tests for isochrones

--- test/isochrones/test.jl
+++ test/isochrones/test.jl
@@ -23,13 +23,42 @@ end
 end
 
 @testset "DownloadIsochroneEzMIST" begin
-    family, age, metal, filter = :mist, 1.0e7, 0.0, "UBVRIplus"
-    df_iso = get_isochrone(family, age, metal, filter)
-    @test typeof(df_iso) == DataFrame
+    n=2
+    exp_l = 6
+    exp_h = 12
+    metal_l = -3.0
+    metal_h = 1.0
+    f_exp(x) = (exp_h-exp_l)*x+exp_l
+    f_metal(x) = (metal_h-metal_l)*x+metal_l
+    for i ∈ 1:n
+        age = 10^f_exp(rand())
+        metal = f_metal(rand())
+        @show age, metal
+        family, age, metal, filter = :mist, age, metal, "UBVRIplus"
+        df_iso = get_isochrone(family, age, metal, filter)
+        @test typeof(df_iso) == DataFrame
+    end
 end
 
 @testset "DownloadIsochroneEzPARSEC" begin
-    family, age, metal, filter = :parsec, 1.0e7, 0.0, "YBC_hsc"
-    df_iso = get_isochrone(family, age, metal, filter)
-    @test typeof(df_iso) == DataFrame
+    n=2
+    exp_l = 6
+    exp_h = 12
+    metal_l = -3.0
+    metal_h = 1.0
+    f_exp(x) = (exp_h-exp_l)*x+exp_l
+    f_metal(x) = (metal_h-metal_l)*x+metal_l
+    for i ∈ 1:n
+        age = 10^f_exp(rand())
+        metal = f_metal(rand())
+        @show age, metal
+        family, age, metal, filter = :parsec, age, metal, "YBC_hsc"
+        df_iso = get_isochrone(family, age, metal, filter)
+        @test typeof(df_iso) == DataFrame
+    end
 end
+# @testset "DownloadIsochroneEzPARSEC" begin
+#     family, age, metal, filter = :parsec, 1.0e7, 0.0, "YBC_hsc"
+#     df_iso = get_isochrone(family, age, metal, filter)
+#     @test typeof(df_iso) == DataFrame
+# end
