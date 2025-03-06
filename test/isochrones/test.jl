66d6cd37b26842c258d0f09c4e765cef2a144563
Martín Mestre <mmestre@fcaglp.unlp.edu.ar>
2025-03-06 15:16:37 -0300

    Now trying to make Grid downloading work.

--- test/isochrones/test.jl
+++ test/isochrones/test.jl
@@ -1,76 +1,87 @@
-@testset "MetallicityTransformationsUsedInMist" begin
-    n = 50
-    for i ∈ 1:n
-        feh = -4 + rand()*(1-(-4))
-        z = feh2z_mist(feh)
-        feh₂ = z2feh_mist(z)
-        @test feh₂ ≈ feh rtol=5.0e-7
-        z₂ = feh2z_mist(feh)
-        @test z₂ ≈ z rtol=5.0e-7
-    end
-end
+# @testset "SSL certificates used by EzPadova" begin
+#     python_exe = ENV["JULIA_PYTHONCALL_EXE"]
+#     env_name = split(python_exe, "/")[end-2]  # El nombre del entorno está en la penúltima posición
+#     ssl_cert_file = joinpath("/home/mmestre/.conda/envs", env_name, "ssl/cert.pem")
+#     ssl_cert_dir = joinpath("/home/mmestre/.conda/envs", env_name, "ssl/certs")
+#     @test ENV["SSL_CERT_FILE"] == ssl_cert_file
+#     @test ENV["SSL_CERT_DIR"] == ssl_cert_dir
+# end
 
-@testset "MetallicityTransformationsUsedInParsec" begin
-    n = 50
-    for i ∈ 1:n
-        feh = -4 + rand()*(1 - (-4))
-        z = feh2z_parsec(feh)
-        feh₂ = z2feh_parsec(z)
-        @test feh₂ ≈ feh rtol=5.0e-5
-        z₂ = feh2z_parsec(feh)
-        @test z₂ ≈ z rtol=5.0e-7
-    end
-end
+# @testset "MetallicityTransformationsUsedInMist" begin
+#     n = 50
+#     for i ∈ 1:n
+#         feh = -4 + rand()*(1-(-4))
+#         z = feh2z_mist(feh)
+#         feh₂ = z2feh_mist(z)
+#         @test feh₂ ≈ feh rtol=5.0e-7
+#         z₂ = feh2z_mist(feh)
+#         @test z₂ ≈ z rtol=5.0e-7
+#     end
+# end
 
-@testset "DownloadIsochroneEzMist" begin
-    n=2
-    exp_l = 9
-    exp_h = 10.3
-    metal_l = -4
-    metal_h = 0.5
-    f_exp(x) = (exp_h-exp_l)*x+exp_l
-    f_metal(x) = (metal_h-metal_l)*x+metal_l
-    for i ∈ 1:n
-        age = 10^f_exp(rand())
-        metal = f_metal(rand())
-        @show age, metal
-        family, age, metal, filter = :mist, age, metal, "UBVRIplus"
-        df_iso = get_isochrone(family, age, metal, filter)
-        @test typeof(df_iso) == DataFrame
-    end
-end
+# @testset "MetallicityTransformationsUsedInParsec" begin
+#     n = 50
+#     for i ∈ 1:n
+#         feh = -4 + rand()*(1 - (-4))
+#         z = feh2z_parsec(feh)
+#         feh₂ = z2feh_parsec(z)
+#         @test feh₂ ≈ feh rtol=5.0e-5
+#         z₂ = feh2z_parsec(feh)
+#         @test z₂ ≈ z rtol=5.0e-7
+#     end
+# end
 
-@testset "DownloadIsochroneEzParsec" begin
+# @testset "DownloadIsochroneEzMist" begin
+#     n=2
+#     exp_l = 9
+#     exp_h = 10.3
+#     metal_l = -4
+#     metal_h = 0.5
+#     f_exp(x) = (exp_h-exp_l)*x+exp_l
+#     f_metal(x) = (metal_h-metal_l)*x+metal_l
+#     for i ∈ 1:n
+#         age = 10^f_exp(rand())
+#         metal = f_metal(rand())
+#         @show age, metal
+#         family, age, metal, filter = :mist, age, metal, "UBVRIplus"
+#         df_iso = get_isochrone(family, age, metal, filter)
+#         @test typeof(df_iso) == DataFrame
+#     end
+# end
+
+# @testset "DownloadIsochroneEzParsec" begin
+#     n=4
+#     exp_l = 9
+#     exp_h = 10.3
+#     metal_l = -2.19999
+#     metal_h = 0.5
+#     f_exp(x) = (exp_h-exp_l)*x+exp_l
+#     f_metal(x) = (metal_h-metal_l)*x+metal_l
+#     for i ∈ 1:n
+#         age = 10^f_exp(rand())
+#         metal = f_metal(rand())
+#         @show age, metal
+#         family, age, metal, filter = :parsec, age, metal, "YBC_hsc"
+#         df_iso = get_isochrone(family, age, metal, filter)
+#         @show typeof(df_iso) == DataFrame
+#     end
+# end
+
+@testset "DownloadIsochroneEzParsec Grid dispatch" begin
     n=4
-    exp_l = 9
-    exp_h = 10.3
-    metal_l = -2.19999
-    metal_h = 0.5
-    f_exp(x) = (exp_h-exp_l)*x+exp_l
-    f_metal(x) = (metal_h-metal_l)*x+metal_l
-    for i ∈ 1:n
-        age = 10^f_exp(rand())
-        metal = f_metal(rand())
-        @show age, metal
-        family, age, metal, filter = :parsec, age, metal, "YBC_hsc"
-        df_iso = get_isochrone(family, age, metal, filter)
-        @show typeof(df_iso) == DataFrame
-    end
-    sys = pyimport("sys")
-    println("Estás usando el entorno de Python en: ", sys.executable)
-    println(pyimport("ssl").get_default_verify_paths())
+    exp_l = 10
+    exp_h = 10.2
+    metal_l = -2.
+    metal_h = 0.3
+    age = (10^exp_l, 10^exp_h, n)
+    metal = (metal_l, metal_h, n)
+    @show age  metal typeof(age) typeof(metal)
+    family, age, metal, filter = :parsec, age, metal, "YBC_hsc"
+    df_iso = get_isochrone(family, age, metal, filter)
+    @show typeof(df_iso) == DataFrame
 end
 
 
-
-@testset "SSL certificates" begin
-    sys = pyimport("sys")
-    println("Estás usando el entorno de Python en: ", sys.executable)
-    println("PYTHONPATH: ", get(ENV, "PYTHONPATH", ""))
-    println("SSL_CERT_FILE: ", get(ENV, "SSL_CERT_FILE", ""))
-    println("SSL_CERT_DIR: ", get(ENV, "SSL_CERT_DIR", ""))
-    println(pyimport("ssl").get_default_verify_paths())
-end
 
 
 
