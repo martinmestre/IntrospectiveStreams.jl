b1db118d827fadb0aa359bae45cb66298fea2d9f
Martín Mestre <mmestre@fcaglp.unlp.edu.ar>
2025-04-03 15:10:10 -0300

    Configuring dict for calling from ezpadova

---
+++ src/config.jl
@@ -0,0 +1,6 @@
+# Diccionario de sistemas fotométricos en Julia
+phot_systems = Dict(
+    "hsc" => "YBC_tab_mag_odfnew/tab_mag_hsc.dat",
+    "lsst" => "YBC_tab_mag_odfnew/tab_mag_lsst.dat",
+    "gaiaEDR3" => "YBC_tab_mag_odfnew/tab_mag_gaiaEDR3.dat"
+)
